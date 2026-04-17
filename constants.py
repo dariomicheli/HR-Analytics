37798e6b7f4623e0377fac446dec5f2ab56e4d37
Ana Julia Nogueira Laspiur <ana.nogueira99@hotmail.com>
2026-04-17 01:31:27 -0300

    probando

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-# Rutas de archivos
+# Rutas de archivos s
 DATA_RAW= "Data/hr_raw.csv"
 DATA_PROCESSED= "Data/processed.csv" # esta la dejo para un futuro, despues ver como lo hacemos si lo metemos en un BD
